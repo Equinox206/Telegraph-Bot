b95f0b323f67e9a674b62a7ff33d2704787ffcf3
Equinox206 <117971229+Equinox206@users.noreply.github.com>
2024-06-19 22:17:42 -0400

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 
 
 class Config(object):
-    TG_BOT_TOKEN = os.environ.get("7370743041:AAECE0lgn062iQ3kIMokbO8OCpaHos9eqtU")
+    TG_BOT_TOKEN = ("7370743041:AAECE0lgn062iQ3kIMokbO8OCpaHos9eqtU")
 
     APP_ID = ("10902915")
 
-    API_HASH = os.environ.get("39d7a7fa8844fe3c75d726475036d7bb")
+    API_HASH = ("39d7a7fa8844fe3c75d726475036d7bb")
